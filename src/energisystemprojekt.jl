03a0db2312ce356d3b1d1d2a1320cdbcd6f192da
Filippa_Hultin <hultinf@student.chalmers.se>
2024-04-08 08:27:46 +0200

    added systemcost as variable

--- src/energisystemprojekt.jl
+++ src/energisystemprojekt.jl
@@ -31,6 +31,7 @@ function buildmodel(input)
         Electricity[r in REGION, p in PLANT, h in HOUR]       >= 0        # MWh/h usage
         Capacity[r in REGION, p in PLANT]                     >= 0        # MW investment
         StoredWater[h in HOUR]                                >= 0        # MWh?
+        Systemcost[r in REGION]                               >= 0        # €
 
     end # variables
 
